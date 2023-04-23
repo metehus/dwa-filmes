64b1fde387608ef40358bef198c702c8f0a0c3c5
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2023-04-23 18:55:25 -0300

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import Planos from './pages/planos';
 import Sobre from './pages/sobre';
 import Detalhes from './pages/detalhes';
 import Cadastrar from './pages/cadastrar';
-import Login from './pages/login';
 
 export default function Routes() {
     return (
@@ -15,7 +14,6 @@ export default function Routes() {
         <Route path='sobre' element={<Sobre />} />
         <Route path='detalhes/:filmeId' element={<Detalhes />} />
         <Route path='cadastrar' element={<Cadastrar />} />
-        <Route path='login' element={<Login />} />
         <Route path='*' element={<h1>Página Não Encontrada!</h1>} />
       </ReactRoutes>
     );
